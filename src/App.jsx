d02642ebef0bf0847c09fe8f049b2a2af5ee10d2
Thejan2006 <hmagthejanbandar@gmail.com>
2026-07-23 23:03:51 +0530

    add i=new routh paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React, { Suspense, lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Toaster } from 'react-hot-toast'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-
-// Pages Lazy Imports
 const HomePage = lazy(() => import('./pages/homePage'))
 const LoginPage = lazy(() => import('./pages/loginPage'))
 const RegisterPage = lazy(() => import('./pages/registerPage'))
 const AdminPage = lazy(() => import('./pages/adminPage'))
 const TestPage = lazy(() => import('./pages/test'))
 const ForgetPasswordPage = lazy(() => import('./pages/forgetPassword'))
-const CheckoutPage = lazy(() => import('./pages/checkout')) // 👈 1. Checkout Page එක Import කළා
-
+import { Toaster } from 'react-hot-toast'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+//357070172721-psc9uchu79ptgs1e12d4gbnli96j29mj.apps.googleusercontent.com
 function App() {
 
   return (
@@ -22,20 +19,18 @@ function App() {
         <Suspense fallback={<div className='w-full h-full flex items-center justify-center'>Loading...</div>}>
           <Routes>
 
+            <Route path='/*'  element={<HomePage/>}  />
+
             <Route path='/signin' element={<LoginPage/>}/>
 
             <Route path='/signup' element={<RegisterPage/>}/>
 
             <Route path='/forget-password' element={<ForgetPasswordPage/>}/>
 
-            <Route path='/checkout' element={<CheckoutPage/>}/> {/* 👈 2. Checkout Route එක එකතු කළා */}
-
             <Route path='/admin/*' element={<AdminPage/>}/>
 
             <Route path='/test' element={<TestPage/>}/>
-
-            {/* /* Route එක හැමතිස්සෙම යටින්ම තියෙන්න ඕන */}
-            <Route path='/*' element={<HomePage/>}/>
+            {/* <Route path="/admin/add-product" element={<AddProductPage />} /> */}
 
           </Routes>
         </Suspense>
