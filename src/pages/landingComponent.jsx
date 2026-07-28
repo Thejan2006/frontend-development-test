38509f10c4f6891ce708ce7786b8eb63b68b63ac
Thejan2006 <hmagthejanbandar@gmail.com>
2026-07-28 19:37:13 +0530

    change home page image

--- src/pages/landingComponent.jsx
+++ src/pages/landingComponent.jsx
@@ -59,7 +59,7 @@ export default function LandingPage() {
             <section
                 className="relative w-full overflow-hidden"
                 style={{
-                    backgroundImage: `url('https://images.unsplash.com/photo-1672211775632-bcb4b68eb2bd?q=80&w=1600&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+                    backgroundImage: `url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
                     backgroundSize: "cover",
                     backgroundPosition: "center 30%",
                 }}
